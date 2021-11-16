4d95c8275aecf84f2233b9ba19ff0006a2e683c9
Evgeniy Lobanov <e.lobanov@corp.101xp.com>
2021-11-16 23:46:00 +0300

    add env

--- web/index.php
+++ web/index.php
@@ -1,11 +1,14 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$dotenv = new Symfony\Component\Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+// comment out the following two lines when deployed to production
+defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_DEBUG']);
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
 
 $config = require __DIR__ . '/../config/web.php';
 
