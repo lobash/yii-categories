3611e109a32eb8205b3b548cad504f6ba3330f1d
Evgeniy Lobanov <e.lobanov@corp.101xp.com>
2021-11-16 21:24:28 +0300

    add postgres in docker-compose.yml

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "pgsql:host={$_ENV['POSTGRES_HOST']};port={$_ENV['POSTGRES_PORT']};dbname={$_ENV['DB_NAME']}",
+    'username' => $_ENV['POSTGRES_USER'],
+    'password' => $_ENV['POSTGRES_PASSWORD'],
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
