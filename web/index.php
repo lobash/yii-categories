4c849c2f645c7bf5e7e8df1f3b041e5a5dc1e25c
Evgeniy Lobanov <e.lobanov@corp.101xp.com>
2021-11-17 00:04:47 +0300

    fix env for configs

--- web/index.php
+++ web/index.php
@@ -1,14 +1,14 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $dotenv = new Symfony\Component\Dotenv\Dotenv();
 $dotenv->load(__DIR__ . '/../.env');
-
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', $_ENV['YII_DEBUG']);
 defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV']);
+
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
